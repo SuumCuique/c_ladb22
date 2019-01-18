7217f46a926a08b3fb14ef58a48782ecee289f82
Vlad <Vlad.V.Kuznetsov@hotmail.com>
2019-01-18 06:03:12 +0200

    fix peak

--- c_ladb22/Main.c
+++ c_ladb22/Main.c
@@ -3,6 +3,33 @@
 #include <string.h>
 #pragma warning(disable: 4996)
 
+int  mas1[250000];
+int  mas2[250000];
+int  mas3[250000];
+int  mas4[250000];
+
+int mas(int index)
+{
+	if (index >= 0 && index < 250000)
+	{
+		return mas1[index];
+	}
+	else if (index >= 250000 && index < 500000)
+	{
+		index -= 250000;
+		return mas2[index];
+	}
+	else if (index >= 500000 && index < 750000)
+	{
+		index -= 500000;
+		return mas3[index];
+	}
+	else if (index >= 750000 && index < 1000000)
+	{
+		index -= 750000;
+		return mas4[index];
+	}
+}
 
 int main()
 {
@@ -11,11 +38,12 @@ int main()
 	scanf("%d", &n);
 	if (n > 0 && n <= 1000000)
 	{
-		int  mas[1000];// = (int*)malloc(n);
-
 		for (int i = 0; i < n; i++)
 		{
-			scanf("%i", &mas[i]);
+			if(i<250000) 	scanf("%i", &mas1[i]);
+			else if (i>=250000 && i< 500000) 	scanf("%i", &mas2[i - 250000]);
+			else if (i >= 500000 && i < 750000) 	scanf("%i", &mas3[i - 500000]);
+			else if (i >= 750000 && i < 1000000) 	scanf("%i", &mas2[i - 750000]);
 		}
 
 		scanf("%i",&m);
@@ -34,15 +62,18 @@ int main()
 						{
 							int prev = 0, next = 0;
 							if (i == 0) prev = 0; else prev = i - 1;
-							if (i == n) next = i; else next = i + 1;
+							if (i == n-1) next = i; else next = i + 1;
 
-							if (mas[i] >= mas[prev] && mas[i] >= mas[next]) count++;
+							if (mas(i) >= mas(prev) && mas(i) >= mas(next)) count++;
 						}
 						printf("%i\n", count);
 					}
 					else if (strcmp(modificator, "UPD") == 0)
 					{
-						mas[arr1] = arr2;
+						if (arr1 < 250000) mas1[arr1] = arr2;
+						else if (arr1 >= 250000 && arr1 < 500000) mas2[arr1 - 250000] = arr2;
+						else if (arr1 >= 500000 && arr1 < 750000) mas3[arr1 - 500000] = arr2;
+						else if (arr1 >= 750000 && arr1 < 1000000) mas2[arr1 - 750000] = arr2;						
 					}
 				}
 			}
